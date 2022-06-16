37deedcb98d92bba2a22d30099e2681eb91d8e20
何忠利 <16955276@qq.com>
2022-06-16 15:25:24 +0800

    1

--- internal/controllers/apiController/home.go
+++ internal/controllers/apiController/home.go
@@ -11,25 +11,7 @@ import (
 )
 
 func HomeSiteOption(c *gin.Context) {
-	// 列表数据
-	var tagsList []pgmodel.Tags
-	pgmodel.DB.Model(&pgmodel.Tags{}).Where("type = $1", 0).Where("is_publish = $2", "yes").Select([]string{"name", "logo", "id"}).Order("sort ASC").Find(&tagsList)
-
-	// 转[]map[string][string] 增加一个全路径图片地址
-	var m []map[string]interface{}
-	imgHost := config.YamlFile.Common.Schema + config.YamlFile.Common.Imghost
-	for _, tags := range tagsList {
-		item := structs.New(tags).Map()
-		if val, ok := item["logo"]; ok {
-			item["logo_url"] = imgHost + fmt.Sprintf("%v", val)
-		}
-		m = append(m, item)
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"e": 0,
-		"m": "操作成功",
-		"d": m,
-	})
+	
 }
 func HomeGetTags(c *gin.Context) {
 	// 列表数据
