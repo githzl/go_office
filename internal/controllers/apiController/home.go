c68e5518042b83d44e00d035c1dee0eb16a40838
何忠利 <16955276@qq.com>
2022-06-18 09:48:11 +0800

    1

--- internal/controllers/apiController/home.go
+++ internal/controllers/apiController/home.go
@@ -17,16 +17,13 @@ func HomeGetTags(c *gin.Context) {
 	// 列表数据
 	var tagsList []pgmodel.Tags
 	pgmodel.DB.Model(&pgmodel.Tags{}).Where("type = $1", 0).Where("is_publish = $2", "yes").Select([]string{"name", "logo", "id"}).Order("sort ASC").Find(&tagsList)
-	
+
+	res := gin.H{"e": 0, "m": "操作成功", "d": make([]int, 0)}
 	if len(tagsList) == 0 {
-		c.JSON(http.StatusOK, gin.H{
-			"e": 0,
-			"m": "操作成功",
-			"d": make([]int, 0),
-		})
+		c.JSON(http.StatusOK, res)
 		return
 	}
-	// 转[]map[string][string] 增加一个全路径图片地址
+	// 转[]map[string]string 返回值增加一个图片全路径地址字段logo_url
 	var m []map[string]interface{}
 	imgHost := config.YamlFile.Common.Schema + config.YamlFile.Common.Imghost
 	for _, tags := range tagsList {
@@ -36,9 +33,6 @@ func HomeGetTags(c *gin.Context) {
 		}
 		m = append(m, item)
 	}
-	c.JSON(http.StatusOK, gin.H{
-		"e": 0,
-		"m": "操作成功",
-		"d": m,
-	})
+	c.JSON(http.StatusOK, res)
+	return
 }
