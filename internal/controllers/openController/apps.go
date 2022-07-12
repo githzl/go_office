72595d9c3aa6870889353b2c3659517586f28795
何忠利 <16955276@qq.com>
2022-07-12 16:13:49 +0800

    1

--- internal/controllers/openController/apps.go
+++ internal/controllers/openController/apps.go
@@ -6,6 +6,11 @@ import (
 )
 
 func AppsMyApps(c *gin.Context) {
+	number := c.Query("number")
+	if number == "" {
+		c.JSON(http.StatusOK, Response.Fail("缺少参数number"))
+		return
+	}
 	c.JSON(http.StatusOK, Response.Succ("hello"))
 	return
 }
